016cea6b317644031ec730901d3f07de683eccc2
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2022-04-25 14:20:43 +0100

    Create semver.rb

---
+++ semver.rb
@@ -0,0 +1,16 @@
+class Semver < Formula
+  desc "Semantic Versioning utility"
+  homepage "https://github.com/chriskilding/semver"
+  url "https://github.com/chriskilding/semver/archive/0.0.10.tar.gz"
+  sha256 "0fac6ee2e4046ea4ae1ca7536157d92d5b189821057a1c97519a51349a9d30bc"
+
+  head "https://github.com/chriskilding/semver.git"
+
+  def install
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    system "#{bin}/semver", "-t", "<<<", "1.0.0"
+  end
+end
